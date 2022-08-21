908fec2f9f12f6a54c1e7782852bc27bade3329b
Avi Saidian <avisaidian@Avis-MacBook-Pro.local>
2022-08-21 18:13:08 +0300

    first draft

---
+++ ReadMe.md
@@ -0,0 +1,9 @@
+# Airflow DAG Generator
+
+Genderating DAG python file for GAD ML flow
+DAG is based on [Jinja Template](https://jinja.palletsprojects.com/)
+
+## Howto Use
+
+1. Update dag_params.json
+2. Execute `python src/generator.py`
---
+++ src/generator.py
@@ -0,0 +1,29 @@
+import os
+from datetime import datetime
+from jinja2 import Environment, PackageLoader, select_autoescape
+import json
+import logging
+
+logger = logging.getLogger().setLevel(logging.DEBUG)
+env = Environment(
+    loader=PackageLoader('airflow'),
+    autoescape=select_autoescape()
+)
+
+target_dir = os.getenv('TARGET_DIR', 'generated')
+conf_file = os.getenv('CONF_FILE', 'dag_params.json')
+with open(conf_file) as cf:
+    conf = json.load(cf)
+    logging.info(f'conf_file: {conf}')
+template = env.get_template("airflow.jinja")
+files = os.listdir('.')
+pynb_files = []
+for file in files:
+    if file.endswith('.ipynb'):
+        pynb_files.append({"path": file, "name": file.replace('.ipynb', '')})
+print(pynb_files)
+
+os.makedirs(target_dir, exist_ok=True)
+with open(f"{target_dir}/airflow_dag.py", 'w') as dag:
+    dag.write(template.render(conf, files=pynb_files, current_date=datetime.now()))
+# print(template.render(files=pynb_files, owner="land", email="land@airflow", interval='@hourly', current_date=datetime.now()))
